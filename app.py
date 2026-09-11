7492ae5b497644264bd62dde20a69f31bcaa740d
sidharth132 <55968842+sidharth132@users.noreply.github.com>
2026-09-11 23:06:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # connection_string = os.getenv("CONNECTION_STRING")
-connection_string = "Driver={ODBC Driver 17 for SQL Server};Server=tcp:lz-prod-sqlserver.database.windows.net,1433;Database=lz-prod-db1;Uid=sqladmin;Pwd=Mysql@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:lz-prod-sqlserver.database.windows.net,1433;Database=lz-prod-db1;Uid=sqladmin;Pwd=Mysql@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 
 app = FastAPI()
 
